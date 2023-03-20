02d38a24563dd0f94e6527fe8e47cc6289daff49
Matthieu MARTINEZ <matthieu.martinez@ecole-isitech.fr>
2023-03-20 17:32:00 +0100

    sd commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+    CloudArrowDownIcon,
+    BookOpenIcon,
+    ChatBubbleLeftEllipsisIcon,
+    CheckBadgeIcon
+} from '@heroicons/react/24/solid'
+
+export const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
